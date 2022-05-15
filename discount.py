d850d5af4c586f1a82abf5aa13e6fdd62242b108
HoChunLin <hochunlin28@gmail.com>
2022-05-15 19:36:06 +0800

    add basic5

--- discount.py
+++ discount.py
@@ -3,12 +3,15 @@ class Discount_counter:
     def price(self, price):
         num_book = len(price)
         each_book_count = [0,0,0,0,0]
+        for i in range(num_book):
+            each_book_count[price[i]] = each_book_count[price[i]] + 1
 
         if num_book == 0:
             return 0
         if num_book == 1:
             return 8
 
-        tmp = sum(i==0 for i in price)
+        tmp = sum(i==0 for i in each_book_count)
+
         if(tmp == 4):
             return 8 * num_book
