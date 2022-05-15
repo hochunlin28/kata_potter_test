f2e5042b96db9ed22e64814df8932d62ab8a5620
HoChunLin <hochunlin28@gmail.com>
2022-05-15 21:20:37 +0800

    pass two edge cases

--- discount.py
+++ discount.py
@@ -5,6 +5,7 @@ class Discount_counter:
         num_book = len(price)
         each_book_count = [0,0,0,0,0]
         total_discount = 0
+        last_is_five = 0
 
         for i in range(num_book):
             each_book_count[price[i]] = each_book_count[price[i]] + 1
@@ -16,23 +17,25 @@ class Discount_counter:
 
         zero_cnt = sum(i==0 for i in each_book_count)        
         while zero_cnt != 5 :
-            tmp = 0
 
             if(zero_cnt == 4):
                 total_discount = total_discount + 8 * 1
             elif(zero_cnt == 3):
                 total_discount = total_discount + 8 * 0.95 * 2
             elif(zero_cnt == 2):
-                total_discount = total_discount + 8 * 0.9 * 3
+                if last_is_five == 1:
+                    total_discount = total_discount - 8 * 0.75* 5 + 2 * (8 * 0.8* 4)
+                else:
+                    total_discount = total_discount + 8 * 0.9 * 3
             elif(zero_cnt == 1):
                 total_discount = total_discount + 8 * 0.8* 4
             elif(zero_cnt == 0):
                 total_discount = total_discount + 8 * 0.75* 5
-                
+                last_is_five = 1
+
             for i in range(5):
                 if each_book_count[i] > 0:
                     each_book_count[i] = each_book_count[i] - 1
-                    tmp = tmp + 1
 
             zero_cnt = sum(i==0 for i in each_book_count)
 
